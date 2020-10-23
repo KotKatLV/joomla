f4342e5368178c04872cfd5fa9a6ea1330c69e59
Константин <kotkat.lv@gmail.com>
2020-10-23 19:55:12 +0300

    Initial commit

---
+++ administrator/logs/error.php
@@ -0,0 +1,7 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2020-10-23 16:15:10 UTC
+#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
+
+#Fields: datetime	priority clientip	category	message
+2020-10-23T16:15:10+00:00	INFO 127.0.0.1	joomlafailure	Empty password not allowed.
